92dc83a8a37125c2fb045c932208799decfb60a5
Nick Sagona <nick@nolainteractive.com>
2017-04-14 15:21:23 -0500

    Revise unit test

--- tests/Build/ParserTest.php
+++ tests/Build/ParserTest.php
@@ -10,7 +10,7 @@ class ParserTest extends \PHPUnit_Framework_TestCase
     public function testGetObjectStreamsAndMap()
     {
         $parser = new Parser();
-        $parser->parse(__DIR__ . '/../tmp/doc.pdf');
+        $parser->parseFile(__DIR__ . '/../tmp/doc.pdf');
         $this->assertTrue(is_array($parser->getObjectStreams()));
         $this->assertTrue(is_array($parser->getObjectMap()));
     }
@@ -19,20 +19,36 @@ class ParserTest extends \PHPUnit_Framework_TestCase
     {
         $this->expectException('Pop\Pdf\Build\Exception');
         $parser = new Parser();
-        $parser->parse(__DIR__ . '/../tmp/bad.pdf');
+        $parser->parseFile(__DIR__ . '/../tmp/bad.pdf');
     }
 
     public function testGetFile()
     {
         $parser = new Parser();
-        $parser->parse(__DIR__ . '/../tmp/doc.pdf');
+        $parser->parseFile(__DIR__ . '/../tmp/doc.pdf');
         $this->assertEquals(__DIR__ . '/../tmp/doc.pdf', $parser->getFile());
     }
 
     public function testGetData()
     {
         $parser = new Parser();
-        $parser->parse(__DIR__ . '/../tmp/doc.pdf');
+        $parser->parseFile(__DIR__ . '/../tmp/doc.pdf');
+        $this->assertContains('%PDF', $parser->getData());
+    }
+
+    public function testGetObjectStreamsAndMapFromData()
+    {
+        $parser = new Parser();
+        $parser->parseData(file_get_contents(__DIR__ . '/../tmp/doc.pdf'));
+        $this->assertTrue(is_array($parser->getObjectStreams()));
+        $this->assertTrue(is_array($parser->getObjectMap()));
+    }
+
+
+    public function testGetDataFromData()
+    {
+        $parser = new Parser();
+        $parser->parseData(file_get_contents(__DIR__ . '/../tmp/doc.pdf'));
         $this->assertContains('%PDF', $parser->getData());
     }
 
